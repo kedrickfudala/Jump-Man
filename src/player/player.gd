10a698c0da1b8d56ccc772ed46e4c1d4fb75dffc
kedrickfudala <KedrickAFudala@gmail.com>
2025-03-21 19:45:56 -0400

    can fall through one way platforms, fixed animations, adjusted movement

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var speed = 30
+@onready var speed = 35
 @onready var jumps = 2
 @onready var dir = 0
 @onready var falling : bool = false
@@ -11,9 +11,19 @@ func handle_input(delta):
 	
 	if is_on_floor():
 		jumps = 2
-	if Input.is_action_just_pressed("ui_up") and jumps > 0:
-		velocity.y = -200
-		jumps -= 1
+		falling = false
+		$JumpTimer.stop()
+	if Input.is_action_pressed("ui_down"):
+		if Input.is_action_pressed("ui_up"):
+			set_collision_mask_value(8, false)
+		else:
+			set_collision_mask_value(8, true)
+	else:
+		if Input.is_action_just_pressed("ui_up") and jumps > 0:
+			velocity.y = -250
+			jumps -= 1
+			falling = false
+			$JumpTimer.stop()
 
 	velocity.x += dir * speed
 	velocity.x *= 0.8
@@ -29,14 +39,13 @@ func handle_anims():
 	if !is_on_floor():
 		if velocity.y < 0:
 			$Sprite.play("jump_up")
-			falling = false
 		if velocity.y > 0:
-			if !falling:
-				$Sprite.play("midair")
-				if !$Sprite.animation == "midair":
-					falling = true
-			else:
+			if $JumpTimer.is_stopped():
+				$JumpTimer.start()
+			if falling:
 				$Sprite.play("fall_down")
+			else:
+				$Sprite.play("midair")
 	elif dir != 0:
 		$Sprite.play("run")
 	else:
@@ -45,3 +54,6 @@ func handle_anims():
 func _physics_process(delta: float) -> void:
 	handle_input(delta)
 	handle_anims()
+
+func _on_jump_timer_timeout() -> void:
+	falling = true
